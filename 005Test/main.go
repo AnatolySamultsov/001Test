e4f81714cb4ab23638dac5455f238b70b50fbcf2
AnatolySamultsov <specmashina.yandex.ru@gmail.com>
2025-04-16 16:01:46 +0100

    files

--- 005Test/main.go
+++ 005Test/main.go
@@ -1,18 +1,16 @@
-# 005Test
 package main
 
-import(
-"bufio"
-"fmt"
-"os"
+import (
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func main() {
-reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 
-fmt.Print("Enter your full name")
-name, _ := reader.ReadString('\n') //includes newline character
-fmt.Println("Your name is", name)
-// fmt.Print(reflect.Type0f(name))
-
-
+	fmt.Print("Enter your full name")
+	name, _ := reader.ReadString('\n') //includes newline character
+	fmt.Println("Your name is", name)
+	// fmt.Print(reflect.Type0f(name))
+}
---
+++ 006/main.go
@@ -0,0 +1,14 @@
+
+
+func main() {
+
+	for i := 0; i < 20; i++ {
+		for i := 0; i < 20
+			if i == 20 {
+				break
+			}
+			fmt.Println(i)
+			
+		}
+	}
+}

712e1cdb623b5f5c3b03716e6183ddb1b67582ec
AnatolySamultsov <specmashina.yandex.ru@gmail.com>
2025-04-17 11:59:54 +0100

    File!

---
+++ 010/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func greet(name string) string {
+	return "Hello," + name + "!"
+}
+func main() {
+
+	message := greet("Jordan")
+
+	fmt.Println(message)
+
+}
